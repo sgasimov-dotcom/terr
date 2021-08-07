500e320acb0d6b44aa556b912a187c19183f0ca2
sgasimov-dotcom <84157053+sgasimov-dotcom@users.noreply.github.com>
2021-08-06 22:24:28 -0500

    Add files via upload

---
+++ farrukh_pre_recorded_Azure_aks_module_build/provider.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+        features {}
+}
+resource "azurerm_resource_group" "example" {
+        name = var.aks_config["resource_group_name"]
+        location = var.aks_config["location"]
+}
